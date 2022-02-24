2881913546405e9f545e15126e7289f89c699e74
YoungCow_code <youngcowcode@gmail.com>
2022-02-24 02:49:22 -0700

    nuxt 이관 완료

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Headline />
+    <Search />
+    <MovieList />
+  </div>
 </template>
 
 <script>
+import Headline from '~/components/Headline'
+import Search from '~/components/Search'
+import MovieList from '~/components/MovieList'
+
 export default {
-  name: 'IndexPage'
+  components: {
+    Headline,
+    Search,
+    MovieList
+  },
+  created() {
+    this.$store.commit('movie/resetMovies')
+  }
 }
 </script>
---
+++ plugins/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import loadImage from './loadImage'
+
+Vue.use(loadImage)
+
